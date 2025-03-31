e96542fd789707673e315c0b7365c6916550940e
teroness <147228831+teroness@users.noreply.github.com>
2025-03-31 18:49:27 +0300

    Update Index.tsx
    Muutettu pelin kuvausta

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,26 +4,17 @@ import Game from '@/components/Game';
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gameColors-navy to-gameColors-darkNavy p-4">
-      <h1 className="text-4xl font-bold mb-6 text-white">
-        <span className="text-gameColors-pink">Piranha</span> Väistelypeli
+  <h1 className="text-4xl font-bold mb-6 text-white">
+  <span className="text-gameColors-pink">Kalapeli</span>
       </h1>
       <p className="text-white mb-8 max-w-lg text-center">
-        Ohjaa Pirhanaa nuolinäppäimillä, kerää ruokaa saadaksesi pisteitä ja väistä koukkuja!
+        Kaikkien kalapelien äiti, jossa sukelletaan työelämän syviin vesiin. Ohjaa Pirhanaa nuolinäppäimillä, kasvata sitä keräämällä ruokaa ja väistä koukkuja!
       </p>
       
       <div className="w-full max-w-4xl">
         <Game />
       </div>
-      
-      <footer className="mt-8 text-sm text-gray-400 text-center">
-        <p>Ohjaa Pirhanaa nuolinäppäimillä, kerää ruokaa ja väistä koukkuja!</p>
-      </footer>
     </div>
-  );
-};
-
-export default Index;
-
   );
 };
 
